4095c930f0f8c8acafada583bcb101a11ae23a41
c18742005 <c18742005@mytudublin.ie>
2021-11-28 16:42:42 +0000

    Added code to allow for multithreading functionality

--- server.c
+++ server.c
@@ -1,30 +1,34 @@
 #include <stdio.h> 
 #include <string.h> 
+#include <stdlib.h>
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <unistd.h> 
+#include <pthread.h>
+#include <stdbool.h>
 
 // Constants
 #define TRUE 1
-#define FALSE 0
-#define MESSAGE_SIZE 500
+#define SOCKET_ERROR -1
+#define MESSAGE_SIZE 4096
 #define PORT 8081
+#define SERVER_BACKLOG 100
+
+void* handle_connection(void* client_socket);
 
 int main(int argc, char* argv[]) {
     // Initialise server variables
     int s; // socket descriptor 
     int cs; // client socket
     int connSize; // size of struct
-    int READSIZE; // size of sockaddr_in for client connection
 
     struct sockaddr_in server, client;
-    char message[MESSAGE_SIZE];
 
     // Create a socket 
     s = socket(AF_INET, SOCK_STREAM, 0);
     
-    if(s == -1) {
-        puts("Could not create socket");
+    if(s == SOCKET_ERROR) {
+        puts("Failed to create socket");
     } else {
         puts("Socket created successfully");
     }
@@ -36,45 +40,65 @@ int main(int argc, char* argv[]) {
 
     // Bind the socket 
     if(bind(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("Bind issue");
-        return 1;
+        perror("Bind error");
     } else {
         puts("Bind complete");
     }
 
     // Listen for a connection
-    listen(s, 3);
-
-    // Await connections
-    puts("Awaiting incoming connection from client >> ");
-    connSize = sizeof(struct sockaddr_in);
-
-    // Accept a connection
-    cs = accept(s, (struct sockaddr *)&client, (socklen_t *)&connSize);
-
-    if(cs < 0) {
-        perror("Can't establish a connection");
-        return 1;
-    } else {
-        puts("Connection from client accepted");
+    if(listen(s, SERVER_BACKLOG) < 0) {
+        perror("Listen error");
     }
+
+    while (true) {
+        // Await connections
+        puts("Awaiting incoming connection from client >> ");
+        connSize = sizeof(struct sockaddr_in);
+
+        // Accept a connection
+        if((cs = accept(s, (struct sockaddr *)&client, (socklen_t *)&connSize)) < 0) {
+            perror("Can't establish a connection");
+        } else {
+            puts("Connection from client accepted");
+        }
+
+        // keep track of thread
+        pthread_t t;
+        int *p_client = malloc(sizeof(int));
+        *p_client = cs;
+
+        pthread_create(&t, NULL, handle_connection, p_client);
+
+    }
+
+    return 0;
+}
+
+void * handle_connection(void* p_client_socket) {
+    char message[MESSAGE_SIZE];
+    int READSIZE; // size of sockaddr_in for client connection
+
+    int client_socket = *((int*)p_client_socket);
+    free(p_client_socket); // no longer needed
 
     // Read data from client
     while(1) {
         memset(message, 0, 500);
-        READSIZE = recv(cs, message, 2000, 0);
+        READSIZE = recv(client_socket, message, 2000, 0);
 
         if(READSIZE == 0) {
             puts("Client disconnected");
             fflush(stdout);
-        } else if(READSIZE == -1){
+        } else if(READSIZE == SOCKET_ERROR){
             perror("Read error");
-            return 1;
+            return NULL;
         }
 
         printf("Client said: %s\n", message);
-        write(cs, "What? ", strlen("What? "));
+        write(client_socket, "What? ", strlen("What? "));
     }
 
-    return 0;
+    close(client_socket);
+    puts("Closing connection");
+    return NULL;
 }
